afd46718f0972d183c0ae30d0a834245b53acb29
Rômulo Henri <123867521+Henri2023@users.noreply.github.com>
2023-02-02 11:23:10 -0300

    Versão 1.0

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var secretNumber = parseInt(Math.random() * 1001)
+
+
+console.log(secretNumber)
+
+function play() {
+    console.log('Tentou!')
+
+    var cn = window.document.getElementById('n')
+    var chosenNumber = Number(cn.value)
+
+    if (secretNumber == chosenNumber) {
+        console.log('Acertou!')
+
+        var change = window.document.getElementById('geniusDefault')
+        change.src='assets/genius-sad.png'
+
+        setTimeout(function() {
+            alert('Você ganhou! Parabéns!')
+        }, 100)
+
+    } else if (secretNumber <= chosenNumber) {
+        alert('Erro! o número é menor.')
+    } else if (secretNumber >= chosenNumber) {
+        alert('Erro! o número é maior.')
+    }
+}
+
+function reload(){
+    window.location.reload();
+} 
