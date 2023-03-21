a99b17e30e7ade0b234b5395ca8ff7d8dab3760d
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-03-21 07:37:02 -0300

    Verão 1.4.0

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # Gênio Numérico
- Adivinhe o número secreto para vencer o gênio misterioso. A lógica é sua forte aliada para vencer o gênio com máxima eficiência no número de tentativas.
+
+Adivinhe o número secreto para vencer o gênio misterioso. A lógica é sua forte aliada para vencer o gênio com máxima eficiência no número de tentativas.
 
 ## Versão 1.0
+
 Versão inicial, número secreto entre 1 e 1.000. (HTML, CSS, JS)
 
 ## Versão 1.1
+
 Versão aprimorada, incrementado sistema de contagem de tentativas para quantificar o desempenho dos jogadores. (HTML, CSS, JS)
 
 ## Versão 1.2
+
 Versão responsiva, programado para ser mobile-friendly. (CSS)
 
 ## Versão 1.3
+
 Resolução de pequenos problemas, tela de vitória e organização dos arquivos como README.md (JS)
+
+## Versão 1.4
+
+Aprimoramento, adicionado o recurso de "record" que salva o melhor desempenho no localStorage.
--- script.js
+++ script.js
@@ -1,137 +1,165 @@
 // Sorteio do Número Secreto
 
-var secretNumber = parseInt(Math.random() * 100)
-const tryTimes = document.getElementById('tryTimes')
-const MSG = document.getElementById('msgSpace')
-const NUM = document.getElementById('numberSpace')
-const TRY = document.getElementById('tryB')
-var tries = 0
-var used = []
+var secretNumber = parseInt(Math.random() * 100);
+const tryTimes = document.getElementById("tryTimes");
+const MSG = document.getElementById("msgSpace");
+const NUM = document.getElementById("numberSpace");
+const TRY = document.getElementById("tryB");
+const INF = document.getElementById("info");
+var tries = 0;
+var used = [];
 
-tryTimes.innerHTML = ('Tentativas: ' + tries)
+tryTimes.innerHTML = "Tentativas: " + tries;
 
-console.log(secretNumber) 
+console.log(secretNumber);
 console.log(used);
 
 // Botão de Tentar
 
 function play() {
-    console.log('Tentou!')
+	console.log("Tentou!");
 
-    var cn = window.document.getElementById('n')
-    var chosenNumber = Number(cn.value)
+	var cn = window.document.getElementById("n");
+	var chosenNumber = Number(cn.value);
 
-    tries = tries + 1
-    tryTimes.innerHTML = ('Tentativas: ' + tries)
+	tries = tries + 1;
+	tryTimes.innerHTML = "Tentativas: " + tries;
 
-    // alert(`Você escolheu tentar com ${chosenNumber}`) // Teste
-    console.log(`Jogador escolheu tentar com ${chosenNumber}`);
+	console.log(`Jogador escolheu tentar com ${chosenNumber}`);
 
-// Cenários de Acertos
+	if (chosenNumber == secretNumber) {
+		// Cenários de Acertos
+		let change = window.document.getElementById("genius");
+		change.src = "assets/genius-sad.png";
 
-    if (chosenNumber == secretNumber) {
-        let change = window.document.getElementById('genius')
-            change.src='assets/genius-sad.png'
+		MSG.innerHTML = `
+            <p>Você venceu o misterioso gênio!</p>`;
+		MSG.style.backgroundColor = "#edc949";
+		MSG.style.color = "rgb(62, 38, 0)";
+		NUM.style.backgroundColor = "#edc949";
+		NUM.style.color = "rgb(62, 38, 0)";
+		NUM.style.fontFamily = "Comicbon";
+		NUM.style.fontSize = "20px";
+		NUM.innerHTML = `Número ${chosenNumber}`;
+		TRY.style.opacity = 0;
+		load();
 
-            MSG.innerHTML = (`
-            <p>Você venceu o misterioso gênio!</p>`)
-            MSG.style.backgroundColor = "#edc949"
-            MSG.style.color = "rgb(62, 38, 0)"
-            NUM.style.backgroundColor = "#edc949"
-            NUM.style.color = "rgb(62, 38, 0)"
-            NUM.style.fontFamily = "Comicbon"
-            NUM.style.fontSize = "20px"
-            NUM.innerHTML = (`Número ${chosenNumber}`)
-            TRY.style.opacity = 0
+		if (tries < localStorage.record) {
+			localStorage.setItem("record", tries);
+		}
 
+		if (tries == 1) {
+			console.log("Acertou!");
 
-        if (tries == 1) {
-            console.log('Acertou!')
+			alert("Você ganhou! com uma única tentativa, uau!");
+		} else if (tries > 1) {
+			var changeHuman = window.document.getElementById("humanDefault");
+			changeHuman.src = "assets/human-default.png";
 
-            alert('Você ganhou! com uma única tentativa, uau!')
+			alert(`Você ganhou! com ${tries} tentativas, Parabéns!`);
+		}
 
-        } else if (tries > 1) {
-            var changeHuman = window.document.getElementById('humanDefault')
-            changeHuman.src='assets/human-default.png'
+		// Cenários de Erros
 
-            alert(`Você ganhou! com ${tries} tentativas, Parabéns!`)
+		// Número Inválido
+	} else if (chosenNumber == NaN || chosenNumber == 0) {
+		alert("Escolha um número para tentar.");
 
-        }
-        
-// Cenários de Erros
+		tries = tries - 1;
 
-// Número Inválido
+		tryTimes.innerHTML = "Tentativas: " + tries;
 
-        } else if (chosenNumber == NaN || chosenNumber == 0) {
-            alert('Escolha um número para tentar.')
+		// Número Fora das Opções
+	} else if (chosenNumber > 100) {
+		alert("O número pensado pelo gênio está entre 1 e 100.");
+		console.log("Número fora das opções.");
 
-            tries = tries - 1
+		tries = tries - 1;
+		tryTimes.innerHTML = "Tentativas: " + tries;
+	} else if (used.includes(chosenNumber)) {
+		console.log("Repetição detectada!");
+		alert("Você já tentou esse número.");
 
-            tryTimes.innerHTML = ('Tentativas: ' + tries)
+		tries = tries - 1;
+		tryTimes.innerHTML = "Tentativas: " + tries;
+	} else if (!used.includes(chosenNumber)) {
+		// Secreto < Escolhido
 
-            // Número Fora das Opções 
+		console.log("Tentativa Nova");
 
-        } else if (chosenNumber > 100) {
+		if (chosenNumber > secretNumber) {
+			alert("Erro! o número escolhido é maior que a resposta.");
+			console.log("Erro! o número escolhido é maior que a resposta.");
 
-            alert('O número pensado pelo gênio está entre 1 e 100.')
-            console.log('Número fora das opções.')
+			if (tries >= 5) {
+				var changeHuman = window.document.getElementById(
+					"humanDefault"
+				);
+				changeHuman.src = "assets/human-not-good.png";
 
-            tries = tries - 1  
-            tryTimes.innerHTML = ('Tentativas: ' + tries)
+				used.push(chosenNumber);
+				used = used.sort(function (a, b) {
+					return a - b;
+				});
+				console.log(used);
+			} else {
+			}
+		} else if (chosenNumber < secretNumber) {
+			// Secreto > Escolhido
 
-        } else if (used.includes(chosenNumber)) {
-            console.log('Repetição detectada!');
-            alert('Você já tentou esse número.')
-            
-            tries = tries - 1  
-            tryTimes.innerHTML = ('Tentativas: ' + tries)
+			alert("Erro! o número escolhido é menor que a resposta.");
+			console.log("Erro! o número escolhido é menor que a resposta.");
 
-        } else if (!used.includes(chosenNumber)) {
+			if (tries >= 5) {
+				var changeHuman = window.document.getElementById(
+					"humanDefault"
+				);
+				changeHuman.src = "assets/human-not-good.png";
+			}
 
-// Secreto < Escolhido
-
-            console.log('Tentativa Nova');
-
-            if (chosenNumber > secretNumber) {
-
-                alert('Erro! o número escolhido é maior que a resposta.')
-                console.log('Erro! o número escolhido é maior que a resposta.')
-
-                if (tries >= 5) {
-                var changeHuman = window.document.getElementById('humanDefault')
-                changeHuman.src='assets/human-not-good.png'
-
-                used.push(chosenNumber)
-                used = used.sort(function(a, b) {return a - b;});
-                console.log(used);} else {}
-
-            } else if (chosenNumber < secretNumber) {
-
-// Secreto > Escolhido
-
-                alert('Erro! o número escolhido é menor que a resposta.')
-                console.log('Erro! o número escolhido é menor que a resposta.')
-        
-                if (tries >= 5) {
-                var changeHuman = window.document.getElementById('humanDefault')
-                changeHuman.src='assets/human-not-good.png'
-                }
-        
-                used.push(chosenNumber)
-                used = used.sort(function(a, b) {return a - b;});
-                console.log(used);                 
-                }
-        }
+			used.push(chosenNumber);
+			used = used.sort(function (a, b) {
+				return a - b;
+			});
+			console.log(used);
+		}
+	}
 }
 
 // Botão de Reiniciar
 
-function reload(){
-    window.location.reload();
-} 
+function reload() {
+	window.location.reload();
+}
 
 // Botão de Continuar
 
+function load() {
+	if (localStorage.record == undefined) {
+		INF.innerHTML = `
+    
+        Seu Record: 0 tentativas.
+    
+    
+    `;
+	} else {
+		if (localStorage.record == 1) {
+			INF.innerHTML = `
+    
+            Seu Record: ${localStorage.record} única tentativa.
+        
+    
+        `;
+		} else {
+			INF.innerHTML = `
+    
+            Seu Record: ${localStorage.record} tentativas.
+        
+    
+        `;
+		}
+	}
+}
 // function win() {
 // console.log('VITÓRIAAAAA');
 // alert('Deu certo')
